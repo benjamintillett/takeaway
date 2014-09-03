83cd35206a49c41800973f8a70e2e4c5ba23b293
benjamintillett <benjamin.tillett@gmail.com>
2014-09-03 07:13:19 +0100

    add Takeaway and and remove dish methods

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,5 +1,17 @@
 class Takeaway
-	def dishes
-		[]
+	
+	attr_reader :dishes
+
+	def initialize
+		@dishes = []
+	end
+
+	def add_dish!(dish)
+		dishes << dish
+		self
+	end
+
+	def remove_dish!(dish)
+		dishes.delete(dish)
 	end
 end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,13 +2,24 @@ require 'takeaway'
 
 describe Takeaway do 
 
+	let(:dominos) { Takeaway.new }
+	let(:chips) { double :dish}
+
 	context "on initialization" do 
 
 		it "has no dishes " do 
-			dominos = Takeaway.new
 			expect(dominos.dishes).to be_empty
 		end
-
 	end 
+
+	it "is able to add dishes" do 
+		dominos.add_dish!(chips)
+		expect(dominos.dishes.first).to be chips
+	end
+	it "is able to remove dishes" do 
+		dominos.add_dish!(chips)
+		dominos.remove_dish!(chips)
+		expect(dominos.dishes).to be_empty
+	end
 
 end
