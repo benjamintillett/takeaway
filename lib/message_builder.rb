2405e73e348442faf0f0fa75cc74d9c8b7738f05
benjamintillett <benjamin.tillett@gmail.com>
2014-09-07 09:13:30 +0100

    add MessageBuilder class

--- lib/message_builder.rb
+++ lib/message_builder.rb
@@ -1,17 +1,21 @@
 class MessageBuilder
 
 	ONE_HOUR_FROM_NOW = (Time.now + 3600).strftime("%H:%M")
-	ORDER_FORMAT =  <<-EOS.gsub(/^[\s\t]*/, '').gsub(/[\s\t]*\n/, ' ').strip
-						Thank you! Your order was placed and 
-						will be delivered before #{ONE_HOUR_FROM_NOW}
-					EOS
-
- 
-  
-
-	def build_message(message)
-
+	FORMAT_HEADER =  "Thank you! Your order was placed and will be delivered before #{ONE_HOUR_FROM_NOW}. "
+	
+	def build_message(order)
+		FORMAT_HEADER + build_order_list(order)
 	end
 
+	def build_order_list(order)
+		order_list = ""
+		puts order.dishes
+		order.dishes.each { |dish, quantity| order_list += format_dish(dish,quantity) }
+		"#{order_list}Total £#{order.total_cost}"
+	end
+
+	def format_dish(dish,quantity)
+		"#{quantity.to_s} x #{dish.name}, "
+	end
 
 end
--- spec/message_builder_spec.rb
+++ spec/message_builder_spec.rb
@@ -4,19 +4,18 @@ describe MessageBuilder do
 
 	let(:fish) { double :dish, name: "fish", price: 4 }
 	let(:chips) { double :dish, name: "chips", price: 2 }
-	let(:bens_order) { double :order, dishes: [chips,fish]}
+	let(:bens_order) { double :order, dishes: { chips => 1, fish => 1}, total_cost: 6}
 	let(:message_builder) { MessageBuilder.new }
 
-	# it "can build a order list" do 
-	# 	expect(message_builder.build_order_list(bens_order)).
-	# 	to eq "1 x chips, 1 x fish. Total £6"
-	# end
+	it "can build a order list" do 
+		expect(message_builder.build_order_list(bens_order)).
+		to eq "1 x chips, 1 x fish, Total £6"
+	end
 
 
-	# it "can build a message" do 
-	# 	expect(message_builder.build_message(bens_order)).
-	# 	to eq "Thank you! Your order was placed and will be delivered before 09:16, 1 x chips, 1 x fish. Total £6"
-
-	# end
+	it "can build a message" do 
+		expect(message_builder.build_message(bens_order)).
+		to match /Thank you! Your order was placed and will be delivered before \d{2}:\d{2}. 1 x chips, 1 x fish. Total £6/
+	end
 
 end
